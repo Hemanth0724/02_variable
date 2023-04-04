b501a8ea0f1cfc259455e6636c472e74a608f4a0
Hemanth0724 <129523049+Hemanth0724@users.noreply.github.com>
2023-04-04 22:27:22 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,11 @@
+# import keyword
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+import keyword as k
+print(k.iskeyword("while"))
+print(k.iskeyword("for"))
+print(k.iskeyword("if"))
+print(k.iskeyword("print"))
+print(k.iskeyword('true'))
+
